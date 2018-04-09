e98507a2be81d3a12195165ada907daeb7349317
Alex Scarlett <alex.scarlett91@gmail.com>
2018-04-08 20:26:13 -0400

    Finished the IMDB component

--- liri.js
+++ liri.js
@@ -7,7 +7,7 @@ var keys = require('./keys.js');
 
 var command = process.argv[2];
 
-var track = process.argv[3];
+var value = process.argv[3];
 
 var spotify = new Spotify(keys.spotify);
 
@@ -28,9 +28,9 @@ if (command === 'my-tweets') {
     }
   });
 } else if (command === 'spotify-this-song') {
-  if (track == null) {
-    track = 'The Sign';
-    spotify.search({ type: 'track', query: track }, function(err, data) {
+  if (value == null) {
+    value = 'The Sign';
+    spotify.search({ type: 'track', query: value }, function(err, data) {
       if (err) throw err;
 
       console.log(
@@ -48,7 +48,7 @@ if (command === 'my-tweets') {
       );
     });
   } else {
-    spotify.search({ type: 'track', query: track }, function(err, data) {
+    spotify.search({ type: 'track', query: value }, function(err, data) {
       if (err) throw err;
 
       console.log(
@@ -67,7 +67,89 @@ if (command === 'my-tweets') {
     });
   }
 } else if (command === 'movie-this') {
-  //
+  if (value == null) {
+    var movieURL = 'http://www.omdbapi.com/?t=Mr+Nobody&apikey=trilogy';
+    request(movieURL, function(error, response, body) {
+      console.log("The movie's title is " + JSON.parse(body).Title + '.');
+      console.log('It was released in ' + JSON.parse(body).Year + '.');
+      console.log(
+        'IMDB gave the movie a rating of ' +
+          JSON.parse(body).imdbRating +
+          ' out of 10.'
+      );
+      console.log(
+        JSON.parse(body).Ratings[1].Source +
+          ' gave the movie a rating of ' +
+          JSON.parse(body).Ratings[1].Value +
+          '.'
+      );
+      console.log(
+        JSON.parse(body).Title +
+          ' was produced in ' +
+          JSON.parse(body).Country +
+          '.'
+      );
+      console.log(
+        JSON.parse(body).Title +
+          ' is available in the following languages: ' +
+          JSON.parse(body).Language +
+          '.'
+      );
+      console.log(
+        'A brief synopsis of ' +
+          JSON.parse(body).Title +
+          ': ' +
+          JSON.parse(body).Plot
+      );
+      console.log(
+        JSON.parse(body).Title +
+          ' features ' +
+          JSON.parse(body).Actors +
+          ' in leading roles.'
+      );
+    });
+  } else {
+    var movieURL = 'http://www.omdbapi.com/?t=' + value + '&apikey=trilogy';
+    request(movieURL, function(error, response, body) {
+      console.log("The movie's title is " + JSON.parse(body).Title + '.');
+      console.log('It was released in ' + JSON.parse(body).Year + '.');
+      console.log(
+        'IMDB gave the movie a rating of ' +
+          JSON.parse(body).imdbRating +
+          ' out of 10.'
+      );
+      console.log(
+        JSON.parse(body).Ratings[1].Source +
+          ' gave the movie a rating of ' +
+          JSON.parse(body).Ratings[1].Value +
+          '.'
+      );
+      console.log(
+        JSON.parse(body).Title +
+          ' was produced in ' +
+          JSON.parse(body).Country +
+          '.'
+      );
+      console.log(
+        JSON.parse(body).Title +
+          ' is available in the following languages: ' +
+          JSON.parse(body).Language +
+          '.'
+      );
+      console.log(
+        'A brief synopsis of ' +
+          JSON.parse(body).Title +
+          ': ' +
+          JSON.parse(body).Plot
+      );
+      console.log(
+        JSON.parse(body).Title +
+          ' features ' +
+          JSON.parse(body).Actors +
+          ' in leading roles.'
+      );
+    });
+  }
 } else if (command === 'do-what-it-says') {
   //
 }
